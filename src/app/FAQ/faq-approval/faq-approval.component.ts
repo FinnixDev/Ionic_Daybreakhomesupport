7a66d44bef94c5073ff6db2e7ae9e7cb5bc09099
FinnixDev <tendaic@finnix.online>
2024-02-17 15:05:07 +0200

    Adding initial commit

---
+++ src/app/FAQ/faq-approval/faq-approval.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-faq-approval',
+  templateUrl: './faq-approval.component.html',
+  styleUrls: ['./faq-approval.component.scss'],
+})
+export class FAQApprovalComponent  implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
---
+++ src/app/FAQ/faq-right-carer/faq-right-carer.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-faq-right-carer',
+  templateUrl: './faq-right-carer.component.html',
+  styleUrls: ['./faq-right-carer.component.scss'],
+})
+export class FAQRightCarerComponent  implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
